5eb08ac975e7782f6e3a5003e986c1cb6c8da79c
pcmarks <peter.c.marks@gmail.com>
2016-08-09 10:53:24 -0400

    upgraded to GenStage 0.50

--- mix.exs
+++ mix.exs
@@ -27,6 +27,6 @@ defmodule GenstageExample2.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:gen_stage, "~> 0.4.3"}]
+    [{:gen_stage, "~> 0.5.0"}]
   end
 end
